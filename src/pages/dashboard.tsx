e2e19c7ce4c74bcca952774dcad0fec911e5d4a9
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2021-04-28 09:55:29 -0300

    Siderbar component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,15 @@
+import { Flex } from "@chakra-ui/layout";
 import { Header } from "../components/Header";
+import { Siderbar } from "../components/Sidebar";
 
 export default function Dashboard() {
   return (
-    <Header></Header>
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" maxW={1480} my="6" mx="auto" px="6">
+        <Siderbar />
+      </Flex>
+    </Flex>
   )
 }
