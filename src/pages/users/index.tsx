17feb1a0911eac047861645b875a84c9661ebc9d
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2021-05-05 16:31:07 -0300

    MirageJs factories e seeds

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -14,19 +14,26 @@ import {
   Link as ChakraLink,
   Text,
   useBreakpointValue
-} from "@chakra-ui/react";
-import Link from 'next/link';
-import { Spinner } from "@chakra-ui/spinner";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { Header } from "../../components/Header";
-import { Pagination } from "../../components/Pagination";
-import Sidebar from "../../components/Sidebar";
+} from "@chakra-ui/react"
+import Link from 'next/link'
+import { useEffect } from 'react'
+import { Spinner } from "@chakra-ui/spinner"
+import { RiAddLine, RiPencilLine } from "react-icons/ri"
+import { Header } from "../../components/Header"
+import { Pagination } from "../../components/Pagination"
+import Sidebar from "../../components/Sidebar"
 
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
   })
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, [])
 
   return (
     <Box>
