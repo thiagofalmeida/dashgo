2c1616558535b51c7f46d217afea192be5bc2fd2
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2021-05-06 11:45:12 -0300

    Prefetch de dados

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,17 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { ReactQueryDevtools } from 'react-query/devtools'
-import { theme } from '../styles/theme'
+import { QueryClientProvider } from 'react-query'
 
-import { QueryClient, QueryClientProvider } from 'react-query'
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
+import { queryClient } from '../services/queryClient'
 import { makeServer } from '../services/mirage'
 
+import { theme } from '../styles/theme'
 
 if (process.env.NODE_ENV === 'development') {
   makeServer()
 }
- 
-const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -9,6 +9,8 @@ import { Pagination } from "../../components/Pagination"
 import Sidebar from "../../components/Sidebar"
 
 import { useUsers } from "../../services/hooks/useUsers"
+import { queryClient } from "../../services/queryClient"
+import { api } from "../../services/api"
 
 export default function UserList() {
   const [page, setPage] = useState(1)
@@ -18,6 +20,16 @@ export default function UserList() {
     base: false,
     lg: true
   })
+
+  async function handlePrefetchUser(userId: string) {
+    await queryClient.prefetchQuery(['user', userId], async () => {
+      const response = await api.get(`users/${userId}`)
+
+      return response.data
+    }, {
+      staleTime: 1000 * 60 * 10 // 10 minutes
+    })
+  }
 
   return (
     <Box>
@@ -76,7 +88,7 @@ export default function UserList() {
                         </Td>
                         <Td>
                           <Box>
-                            <ChakraLink color="purple.400">
+                            <ChakraLink color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
                               <Text fontWeight="bold">{user.name}</Text>
                             </ChakraLink>
                             <Text fontSize="sm" color="gray.300">{user.email}</Text>
